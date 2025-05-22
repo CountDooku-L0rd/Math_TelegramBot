e3e16278940e5c42148b50c8f894378135582f64
CountDooku_l0rd <plesakovartem28@gmail.com>
2025-05-22 13:52:52 +0300

    Создано подключение к удалённой базе данных MySQL

---
+++ bot/utils/db.py
@@ -0,0 +1,9 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DB_URL = f"mysql+pymysql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}/{os.getenv("DB_NAME")}?charset=utf8"
+
+engine = create_engine(DB_URL, echo=False)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
